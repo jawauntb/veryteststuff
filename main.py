ee6d1f1c1211a1a7a837f077795f8265928e8ee3
jawauntb <jawaun.brown95@gmail.com>
2023-05-08 19:43:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+from collections import Counter
+
+def get_total_scores(user, score_map):
+  ENDPOINT = 'https://api.github.com/users/{}/events'.format(user)
+  payload = requests.get(ENDPOINT).json()
+  total_score = 0
+  list_of_events = [event['type'] for event in payload]
+  c = Counter(list_of_events)
+  for key in c:
+    if key in score_map:
+      score = score_map[key] * c[key]
+      total_score += score
+    else:
+      total_score += c[key]
+  return total_score
+  
+score_map = {
+    'PullRequestEvent':5,
+    'ForkEvent':4, 
+    'IssueCommentEvent':3,
+    'PushEvent':2,
+    'other':1
+}
+print(get_total_scores('danielspofford', score_map))
